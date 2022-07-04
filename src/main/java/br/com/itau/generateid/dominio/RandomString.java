abbc33fb456cbcc6a45c17cb4242c1d1a8f944e9
mrissexavier <m.risse.dev@gmail.com>
2022-07-04 14:59:54 -0300

    Refactored

--- src/main/java/br/com/itau/generateid/dominio/RandomString.java
+++ src/main/java/br/com/itau/generateid/dominio/RandomString.java
@@ -5,33 +5,34 @@ import java.io.FileReader;
 
 public class RandomString {
     public static void main(String[] args) {
-        String nomeUsuario = new String();
+        String nomeUsuario;
         String nomeArquivo = "C:\\Users\\EmilyOliveiraXavier\\IdeaProjects\\GenerateID\\Massa de Dados.txt";
 
         File arq = new File(nomeArquivo);
 
-        if(arq.exists()){
+        if (arq.exists()){
             try {
                 FileReader leitorArquivos = new FileReader(nomeArquivo);
                 BufferedReader bufferedReader = new BufferedReader(leitorArquivos);
 
-                while(true){
+                while (true){
                     nomeUsuario = bufferedReader.readLine();
                     nomeUsuario = nomeUsuario.replaceAll(" de "," ");
                     nomeUsuario = nomeUsuario.replaceAll(" da ", " ");
                     String[] loginUsuario = nomeUsuario.split(" ");
 
-                    if(nomeUsuario == null) {break;}
+                    if (nomeUsuario == null) {break;}
 
                     System.out.println("Usuario: " +nomeUsuario.toUpperCase());
 
-                    if(loginUsuario[0].length() >= 7 || loginUsuario[1].length() >= 3){
-                        System.out.println("Login: " + loginUsuario[0].substring(0,4).toUpperCase()+
-                                loginUsuario[1].substring(0,3).toUpperCase()+
-                                "\n===================================");
+                    if (loginUsuario[0].length() >= 7 || loginUsuario[1].length() >= 3){
+                        System.out.println("Login: "
+                                + loginUsuario[0].substring(0,4).toUpperCase()
+                                + loginUsuario[1].substring(0,3).toUpperCase()
+                                + "\n-----------------------------------------");
                     }
                 }
-            } catch (Exception e){}
+            } catch (Exception e) {}
         }
     }
 }
